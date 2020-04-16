d66dfa6dc055d2c8db7640ddcf3ad66ff3eed69d
Janessa Garrow <janessa.garrow@gmail.com>
2020-04-15 23:44:24 -0600

    Add GET requests for shopping list and instructions

--- routes/recipe_routes.js
+++ routes/recipe_routes.js
@@ -11,4 +11,24 @@ router.get("/recipes", (req, res) => {
         );
 });
 
+router.get("/recipes/:id/shoppingList", (req, res) => {
+    const { id } = req.params;
+
+    RecipeSchemes.getShoppingList(id)
+        .then((shoppingList) => res.status(200).json(shoppingList))
+        .catch((err) =>
+            res.status(500).json({ message: "Error fetching shopping list" })
+        );
+});
+
+router.get("/recipes/:id/instructions", (req, res) => {
+    const { id } = req.params;
+
+    RecipeSchemes.getInstructions(id)
+        .then((instructions) => res.status(200).json(instructions))
+        .catch((err) =>
+            res.status(500).json({ message: "Error fetching instructions" })
+        );
+});
+
 module.exports = router;
